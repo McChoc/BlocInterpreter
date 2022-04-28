50420014a63d4ad4dfd75091bad09fbd60c19e8a
McChoc <57012414+McChoc@users.noreply.github.com>
2022-04-28 14:38:27 -0400

    Initial commit

---
+++ Example/ConsoleColor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+static class ConsoleColor
+{
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern bool SetConsoleMode(IntPtr hConsoleHandle, int mode);
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern bool GetConsoleMode(IntPtr handle, out int mode);
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern IntPtr GetStdHandle(int handle);
+
+    static ConsoleColor()
+    {
+        var handle = GetStdHandle(-11);
+        GetConsoleMode(handle, out int mode);
+        SetConsoleMode(handle, mode | 0x4);
+        Console.ResetColor();
+    }
+
+    public static void SetColor (byte color)
+    {
+        Console.Write($"\x1b[38;5;{color}m");
+    }
+}
---
+++ Example/Program.cs
@@ -0,0 +1,85 @@
+﻿using CmmInterpretor;
+using CmmInterpretor.Commands;
+using CmmInterpretor.Data;
+using CmmInterpretor.Exceptions;
+using CmmInterpretor.Results;
+using CmmInterpretor.Values;
+using System.Collections.Generic;
+using System.Linq;
+using Console = System.Console;
+
+const byte RED = 9;
+const byte ORANGE = 208;
+
+List<string> lines = new();
+
+Engine engine = new Engine.Builder(args)
+    .OnLog(msg => Console.WriteLine(msg))
+    .OnClear(() => Console.Clear())
+    .AddDefaultCommands()
+    .Build();
+
+while (true)
+{
+    int a = 0;
+
+    try
+    {
+        Console.Write("> ");
+        string line = Console.ReadLine();
+
+        bool doExecute = true;
+
+        if (line.LastOrDefault() == '\\')
+        {
+            line = line[0..^1];
+            doExecute = false;
+        }
+
+        lines.Add(line);
+
+        if (doExecute)
+        {
+            string code = string.Join("\n", lines);
+            lines.Clear();
+
+            if (code[^1] != ';')
+                code += ';';
+
+            var result = engine.Execute(code);
+
+            if (result is IValue value)
+            {
+                if (value.Value() is not Void)
+                    Console.WriteLine(value.Value().ToString());
+            }
+            else if (result is Return || result is Exit)
+            {
+                break;
+            }
+            else if (result is Throw t)
+            {
+                ConsoleColor.SetColor(ORANGE);
+                Console.WriteLine($"An exception was thrown : {t.value}");
+                Console.WriteLine();
+                Console.ResetColor();
+            }
+
+            Console.WriteLine();
+        }
+    }
+    catch (SyntaxError e)
+    {
+        ConsoleColor.SetColor(RED);
+        Console.WriteLine($"Syntax error : {e.Message}");
+        Console.WriteLine();
+        Console.ResetColor();
+    }
+    //catch
+    //{
+    //    ConsoleColor.SetColor(RED);
+    //    Console.WriteLine("An unexpected error occured");
+    //    Console.WriteLine();
+    //    Console.ResetColor();
+    //}
+}
